6f66fbfaa27addf6fc43b1257f8610ff44765396
meg <meg@meg-virtual-machine.(none)>
2013-04-16 21:28:14 +0800

    初始化导入空白项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moonsnack::Application.config.secret_token = '9303b691c03e62b9929d63279ea7c36f31f8dfd7a2f46b3c4767ef0a1c7b45e1315aa50e343a0556b307ed09152c38775e36dc7117a19756c61cce1c91ddc69a'
